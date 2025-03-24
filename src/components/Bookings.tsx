4dab28d23a4516545fc42948ca8767d99f3b0ff1
SatanAlphabet <ori0pppppp@gmail.com>
2025-03-24 21:14:10 +0700

    admin can now view user ID on bookings

--- src/components/Bookings.tsx
+++ src/components/Bookings.tsx
@@ -7,11 +7,12 @@ export default function Bookings({booking, admin}:{booking: Booking, admin:Boole
     const bookDate = new Date(booking.bookingDate);
 
     return (
-        <div className="bg-gray-100 w-full px-4 py-2 my-2 grid grid-cols-2 justify-items-start items-center">
+        <div className="bg-gray-100 w-full px-4 py-2 my-2 grid grid-cols-2 justify-items-start items-center rounded">
             <div>
                 <div>Dentist: {booking.dentist.name}</div>
                 <div suppressHydrationWarning={true}>Date: {bookDate.toDateString()}</div>
-                {admin && <div>ID: {booking._id}</div>}
+                {admin && <><div className="text-[12px]">ID: {booking._id}</div>
+                <div className="text-[12px]">User: {booking.user}</div></>}
             </div>
     
             <div className="h-[50%] w-[80%] justify-self-end grid grid-cols-2 gap-3">
